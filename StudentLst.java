ba34d08e1d7a5d1ce02f058ec523bf24a93cb48a
JayRMagee <115039006+JayRMagee@users.noreply.github.com>
2023-01-31 11:59:16 -0500

    Add files via upload
    This is the module 2 assignment with the ToDos completed. Created new repository for it.

---
+++ StudentLst.java
@@ -0,0 +1,36 @@
+package com.mycompany.studentlst;
+
+import java.util.Scanner;
+
+/**
+ * @author MoaathAlrajab
+ */
+public class StudentLst {
+
+    public static void main(String[] args) {
+        Student[] students= new Student[2];
+        Scanner inScanner = new Scanner(System.in);
+        
+        for(int i=0; i< students.length;i++){
+            System.out.print("Enter student's name: ");
+            String name = inScanner.nextLine();
+            Student student = new Student(name);
+            
+            students[i]= student; 
+            
+        }
+        System.out.println(printOutput(students));
+    }
+    
+    public static String printOutput(Student[] students){
+        String str="Printing the output ";
+        
+        //To Do 03: complete the implementation of this method  
+        for(int i = 0; i < students.length; i++) {
+            str += "The student's name is " + students[i].getName() + " \n";
+        }//check
+        return str;
+    }
+    
+    
+}
